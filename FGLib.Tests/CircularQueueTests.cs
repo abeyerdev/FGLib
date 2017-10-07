318ea1c63906c1d551c1b4fb4435732623d356f0
Adam Beyer <abeyerdev@gmail.com>
2017-10-07 15:35:45 -0500

    initial commit for FGLib

---
+++ FGLib.Tests/CircularQueueTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircularQueue.Tests
+{
+    [TestClass]
+    public class CircularQueueTests
+    {
+        [TestMethod]
+        public void CanConstruct_Test()
+        {
+            CircularQueue<string> queue = new CircularQueue<string>(5);
+            Assert.IsInstanceOfType(queue, typeof(CircularQueue<string>));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Need to provide a size of at least 2 for a CircularQueue.")]
+        public void CannotCreateQueueSmallerThan2_Test()
+        {
+            CircularQueue<string> failure = new CircularQueue<string>(1);
+        }
+
+        [TestMethod]
+        public void CanAdd_Test()
+        {
+            CircularQueue<string> queue = new CircularQueue<string>(5);
+            queue.Add("First");
+            queue.Add("Second");
+            queue.Add("Third");
+            queue.Add("Fourth");
+            queue.Add("Fifth");
+
+            Assert.IsTrue(queue[0] == "First");
+            Assert.IsTrue(queue[1] == "Second");
+            Assert.IsTrue(queue[2] == "Third");
+            Assert.IsTrue(queue[3] == "Fourth");
+            Assert.IsTrue(queue[4] == "Fifth");
+
+            Assert.IsTrue(queue.Head == 0);
+            Assert.IsTrue(queue.Tail == 4);
+        }
+
+        [TestMethod]
+        public void CanAddToLoopAround_Test()
+        {
+            CircularQueue<string> queue = new CircularQueue<string>(5);
+            queue.Add("First");
+            queue.Add("Second");
+            queue.Add("Third");
+            queue.Add("Fourth");
+            queue.Add("Fifth");
+            queue.Add("Sixth");
+            queue.Add("Seventh");
+            queue.Add("Eighth");
+            queue.Add("Ninth");
+            queue.Add("Tenth");
+            queue.Add("Eleventh");
+
+            Assert.IsTrue(queue[0] == "Eleventh");
+            Assert.IsTrue(queue[1] == "Seventh");
+            Assert.IsTrue(queue[2] == "Eighth");
+            Assert.IsTrue(queue[3] == "Ninth");
+            Assert.IsTrue(queue[4] == "Tenth");
+
+            Assert.IsTrue(queue.Head == 1);
+            Assert.IsTrue(queue.Tail == 0);
+        }
+
+        [TestMethod]
+        public void CanRemove_Test()
+        {
+            CircularQueue<string> queue = new CircularQueue<string>(5);
+            queue.Add("First");
+            queue.Add("Second");
+            queue.Add("Third");
+            queue.Add("Fourth");
+            queue.Add("Fifth");
+            queue.Remove();
+
+            Assert.IsTrue(queue[0] == null);
+            Assert.IsTrue(queue[1] == "Second");
+            Assert.IsTrue(queue[2] == "Third");
+            Assert.IsTrue(queue[3] == "Fourth");
+            Assert.IsTrue(queue[4] == "Fifth");
+
+            Assert.IsTrue(queue.Head == 1);
+            Assert.IsTrue(queue.Tail == 4);
+        }
+
+        [TestMethod]
+        public void CanClear_Test()
+        {
+            CircularQueue<int> queue = new CircularQueue<int>(3) { 0, 222, 2321 };
+            queue.Clear();
+
+            Assert.IsTrue(queue.IsEmpty());
+        }
+
+    }
+}
---
+++ FGLib/CircularQueue.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CircularQueue
+{
+    /// <summary>
+    /// A circular queue to hold input information
+    /// for FGLib InputBuffer.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CircularQueue<T> : IEnumerable<T>
+    {
+        private T[] _queue;
+        private int _head, _tail = 0;
+        private int _size;
+
+        public int Head { get { return _head; } }
+        public int Tail { get { return _tail; } }
+        public int Size { get { return _size; } }
+
+        public CircularQueue(int size)
+        {
+            if (size < 2)
+            {
+                throw new ArgumentOutOfRangeException("Need to provide a size of at least 2 for a CircularQueue.");
+            }
+
+            _size = size;
+            _queue = new T[_size];
+        }
+
+        public T this[int i]
+        {
+            get { return _queue[i]; }
+        }
+
+        /// <summary>
+        /// Adds an element to end/tail of the queue.
+        /// </summary>
+        /// <param name="element"></param>
+        public void Add(T element)
+        {
+            if (isIndexClear(_head))
+            {
+                _queue[_head] = element;
+            }
+            else
+            {
+                _tail = (_tail + 1) % _size;
+                _queue[_tail] = element;
+
+                // handle case when tail becomes head
+                if (_head == _tail)
+                {
+                    _head = (_head + 1) % _size;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes an element from the front/head of the queue.
+        /// </summary>
+        public void Remove()
+        {
+            if (!IsEmpty())
+            {
+                _queue[_head] = default(T);
+                _head = (_head + 1) % _size;
+            }
+        }
+
+        /// <summary>
+        /// Clear the queue of all current values.
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                _queue[i] = default(T);
+            }
+
+            _head = _tail;
+        }
+
+        public bool IsEmpty()
+        {
+            return _head == _tail;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_queue).GetEnumerator();
+        }
+
+        private bool isIndexClear(int index)
+        {
+            return _queue[index] == null || _queue[index].Equals(default(T));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)_queue).GetEnumerator();
+        }
+    }
+}
